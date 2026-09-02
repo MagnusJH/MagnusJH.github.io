d7e9982a9824c78031d28429936d0746e7906b57
MagnusJH <123589587+MagnusJH@users.noreply.github.com>
2026-09-02 19:44:23 +0200

    added run command

--- VideoToASCII.py
+++ VideoToASCII.py
@@ -117,5 +117,6 @@ def process_data():
     return take_video(video_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
